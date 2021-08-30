f0d74529099a2bc5ef53ac0359257ffdd763533a
Vlad Tumilovich <vladislav.tumilovich@gmail.com>
2021-08-30 22:59:43 +0200

    56. Set & Clear Current Contact

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,19 @@
+import React, { Fragment, useContext } from 'react';
+import ContactItem from './ContactItem';
+import ContactContext from '../../context/contact/contactContext';
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts } = contactContext;
+
+  return (
+    <Fragment>
+      {contacts.map((contact) => {
+        return <ContactItem key={contact.id} contact={contact} />;
+      })}
+    </Fragment>
+  );
+};
+
+export default Contacts;
